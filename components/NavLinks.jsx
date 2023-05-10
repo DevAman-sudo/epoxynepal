b3df258cd437a9b33da500a205ffedb1c281dd76
DevAman-sudo <devaman.net@gmail.com>
2023-05-10 07:42:55 +0530

    our projects

--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -134,6 +134,16 @@ const NavLinks = () => {
             Category
           </a>
         </li>
+
+        <li className="mt-4 md:mt-0 border-b border-gray-400 md:border-none pb-2 md:pb-0">
+          <a
+            className={styles.menuItems}
+            href="/admin/projects"
+            aria-current="page"
+          >
+            Projects
+          </a>
+        </li>
       </>
     );
   } else {
--- components/ShopByArt.jsx
+++ components/ShopByArt.jsx
@@ -1,27 +1,50 @@
-import React from "react";
-import ShopByArtCard from "./ShopByArtCard";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const ShopByArt = () => {
-  return (
-    <>
-      <div className=" bg-pink py-4 w-screen mx-auto">
-        <div className="flex w-full h-auto text-gray-700">
-          <h1 className="mt-auto font-bold float-left m-4 text-3xl ">
-            Shop By Art
-          </h1>
-          <p className=" mt-auto font-bold ml-auto m-4 underline">view all</p>
-        </div>
+  const [categoryData, setCategoryData] = useState([]);
 
-        <div className="mx-8 flex justify-evenly flex-wrap">
-          <ShopByArtCard/>
-          <ShopByArtCard/>
-          <ShopByArtCard/>
-          <ShopByArtCard/>
-          <ShopByArtCard/>
-          <ShopByArtCard/>
-        </div>
+  // get category
+  const getCategory = async () => {
+    try {
+      const categoryDatas = await axios.get("/api/admin/projects");
+      setCategoryData(categoryDatas.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getCategory();
+  }, []);
+
+  return (
+    <div className="bg-pink py-4">
+      <div className="flex w-full h-auto text-gray-700">
+        <h1 className="mt-auto font-bold float-left m-4 text-3xl">
+          Our Projects
+        </h1>
       </div>
-    </>
+
+      <div className="flex justify-left overflow-scroll">
+        {categoryData.map((category, index) => (
+          
+            <div key={index} className=" shadow-md m-2 p-4 rounded-md">
+              <div className="w-24 h-24 md:w-40 md:h-40  mx-auto overflow-hidden">
+                <img
+                  className="w-full h-full object-cover p-4"
+                  src={category.image}
+                  alt={category.category}
+                />
+              </div>
+              <div className="text-center mt-2">
+                <h2 className="font-700 text-sm sm:text-base tracking-widest">
+                  {category.category}
+                </h2>
+              </div>
+            </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
